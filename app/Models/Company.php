9b3d355bfa1013e2476e18737b3f4a8b6aac56c4
Nikita Antonov <nikita45454@gmail.com>
2024-05-09 22:55:21 +0500

    feat: настраивает связи для моделей: Company, Operation, Product

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Company extends Model
 {
@@ -15,4 +16,9 @@ class Company extends Model
         'email',
         'phone',
     ];
+
+    public function operations(): HasMany
+    {
+        return $this->hasMany(Operation::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -12,4 +13,8 @@ class Product extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function operations(): BelongsToMany {
+        return $this->belongsToMany(Operation::class);
+    }
 }
