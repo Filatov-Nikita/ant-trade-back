e58091e64ffb90592702d75bba698f43474562ad
Nikita Antonov <nikita45454@gmail.com>
2024-05-09 20:09:49 +0500

    feat: Company - добавляет модель и классы для базы

--- database/migrations/2024_05_08_160842_create_companies_table.php
+++ database/migrations/2024_05_08_160842_create_companies_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->text('description');
+            $table->string('phone', 64);
+            $table->string('email', 128);
             $table->timestamps();
         });
     }
