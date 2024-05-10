e62521e8d8af6d4e1bebd4af7ce6e7dea2b2d1da
Nikita Antonov <nikita45454@gmail.com>
2024-05-10 11:48:31 +0500

    feat: компании - добавляет контроллер, реквесты и ресурсы

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Company;
+use App\Http\Requests\StoreCompanyRequest;
+use App\Http\Requests\UpdateCompanyRequest;
+use App\Http\Resources\CompanyResource;
+use App\Http\Resources\CompanyShowResource;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CompanyResource::collection(Company::latest()->paginate(16));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCompanyRequest $request)
+    {
+        $company = Company::create($request->validated());
+
+        return new CompanyResource($company);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Company $company)
+    {
+        return new CompanyShowResource($company);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCompanyRequest $request, Company $company)
+    {
+        $company->fill($request->validated())->save();
+
+        return new CompanyResource($company);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Company $company)
+    {
+        $company->delete();
+    }
+}
