8c7188a3b6ee38404dc26bcf57df88720b0b7b3b
Nikita Antonov <nikita45454@gmail.com>
2025-03-31 12:28:08 +0500

    feat: auth - закрывает апи маршруты через sanctum

--- routes/api.php
+++ routes/api.php
@@ -8,28 +8,30 @@ use App\Http\Controllers\OperationCashController;
 use App\Http\Controllers\OperationProductController;
 use App\Http\Controllers\OperationController;
 use App\Http\Controllers\FileController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/auth/user', [AuthController::class, 'showUser'])->name('auth.user');
 
-Route::apiResources([
-    'companies' => CompanyController::class,
-    'products' => ProductController::class,
-]);
+    Route::apiResources([
+        'companies' => CompanyController::class,
+        'products' => ProductController::class,
+    ]);
 
-Route::prefix('operations')
-->name('operations.')
-->group(function() {
-    Route::post('/cash', [OperationCashController::class, 'store'])->name('cash.store');
-    Route::put('/{operation}/cash', [OperationCashController::class, 'update'])->name('cash.update');
-    Route::post('/products', [OperationProductController::class, 'store'])->name('products.store');
-    Route::put('/{operation}/products', [OperationProductController::class, 'update'])->name('products.update');
-    Route::get('/balance', [OperationController::class, 'index'])->name('balance');
-    Route::get('/{operation}', [OperationController::class, 'show'])->name('show');
-    Route::delete('/{operation}', [OperationController::class, 'destroy'])->name('destroy');
+    Route::prefix('operations')
+    ->name('operations.')
+    ->group(function() {
+        Route::post('/cash', [OperationCashController::class, 'store'])->name('cash.store');
+        Route::put('/{operation}/cash', [OperationCashController::class, 'update'])->name('cash.update');
+        Route::post('/products', [OperationProductController::class, 'store'])->name('products.store');
+        Route::put('/{operation}/products', [OperationProductController::class, 'update'])->name('products.update');
+        Route::get('/balance', [OperationController::class, 'index'])->name('balance');
+        Route::get('/{operation}', [OperationController::class, 'show'])->name('show');
+        Route::delete('/{operation}', [OperationController::class, 'destroy'])->name('destroy');
+    });
+
+    Route::post('/upload-file', [FileController::class, 'store'])->name('file.upload');
+    Route::delete('/remove-file/{file}', [FileController::class, 'destroy'])->name('file.destroy');
 });
-
-Route::post('/upload-file', [FileController::class, 'store'])->name('file.upload');
-Route::delete('/remove-file/{file}', [FileController::class, 'destroy'])->name('file.destroy');
